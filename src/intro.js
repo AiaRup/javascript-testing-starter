dcbad33c59897fbe7864f541c503448763b9b3d2
Aia Rupsom <aia@uelzpay.com>
2024-11-20 14:10:12 +0200

    feat: add unit tests for calculate average and factorial functions

--- src/intro.js
+++ src/intro.js
@@ -10,3 +10,14 @@ export function fizzBuzz(n) {
   if (n % 5 === 0) return "Buzz";
   return n.toString();
 }
+
+export function calculateAverage(numbers) {
+  if (numbers.length === 0) return NaN;
+  return numbers.reduce((sum, current) => sum + current, 0) / numbers.length;
+}
+
+export function calculateFactorial(n) {
+  if (n < 0) return undefined;
+  if (n === 0 || n === 1) return 1;
+  return n * calculateFactorial(n - 1);
+}
--- src/tests/intro.test.js
+++ src/tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it, test } from "vitest";
-import { fizzBuzz, max } from "../intro";
+import { fizzBuzz, max, calculateAverage, calculateFactorial } from "../intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -30,5 +30,49 @@ describe("fizzBuzz", () => {
 
   it("should return the argument as a string if it is not divisible by 3 or 5", () => {
     expect(fizzBuzz(4)).toBe("4");
+  });
+});
+
+describe("calculateAverage", () => {
+  test("should return NaN if given an empty array", () => {
+    expect(calculateAverage([])).toBe(NaN);
+  });
+
+  test("should calculate the average of an array with a single argument", () => {
+    expect(calculateAverage([1])).toBe(1);
+  });
+
+  test("should calculate the average of an array with two arguments", () => {
+    expect(calculateAverage([1, 2])).toBe(1.5);
+  });
+
+  test("should calculate the average of an array with three arguments", () => {
+    expect(calculateAverage([1, 2, 3])).toBe(2);
+  });
+});
+
+describe("calculateFactorial", () => {
+  it("should return 1 if given 0 ", () => {
+    expect(calculateFactorial(0)).toBe(1);
+  });
+
+  it("should return 1 if given 1 ", () => {
+    expect(calculateFactorial(1)).toBe(1);
+  });
+
+  it("should return 2 if given 2 ", () => {
+    expect(calculateFactorial(2)).toBe(2);
+  });
+
+  it("should return 6 if given 3 ", () => {
+    expect(calculateFactorial(3)).toBe(6);
+  });
+
+  it("should return 24 if given 4 ", () => {
+    expect(calculateFactorial(4)).toBe(24);
+  });
+
+  it("should return undefined if given a negative number ", () => {
+    expect(calculateFactorial(-1)).toBeUndefined();
   });
 });
