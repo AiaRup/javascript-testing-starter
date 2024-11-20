d4047bbaa030b15bceb9beafe67aa11d22f103da
Aia Rupsom <aia@uelzpay.com>
2024-11-20 13:40:06 +0200

    feat: first unit test for fizzbuzz function

--- src/tests/intro.test.js
+++ src/tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, expect, it, test } from "vitest";
-import { max } from "../intro";
+import { fizzBuzz, max } from "../intro";
 
 describe("max", () => {
   it("should return the first argument if it is greater", () => {
@@ -12,5 +12,23 @@ describe("max", () => {
 
   it("should return the first argument if arguments are equal", () => {
     expect(max(1, 1)).toBe(1);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("should return FizzBuzz if the argument is divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("should return Fizz if the argument is only divisible by 3", () => {
+    expect(fizzBuzz(6)).toBe("Fizz");
+  });
+
+  it("should return Buzz if the argument is only divisible by 5", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+
+  it("should return the argument as a string if it is not divisible by 3 or 5", () => {
+    expect(fizzBuzz(4)).toBe("4");
   });
 });
